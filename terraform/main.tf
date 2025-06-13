f02d94a62073ca281addfd1d1bc5e5ae9c356860
Patel Rajan <76256243+PatelRajan-2481@users.noreply.github.com>
2025-06-13 12:56:05 -0400

    Update main.tf
    Remove IAM Role code snippet

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,31 +71,6 @@ resource "aws_ecr_repository" "mysql" {
   force_delete = true
 }
 
-# # IAM Role for EC2 to access ECR
-# resource "aws_iam_role" "ec2_ecr_role" {
-#   name = "ec2-ecr-access-role"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Effect = "Allow",
-#       Principal = {
-#         Service = "ec2.amazonaws.com"
-#       },
-#       Action = "sts:AssumeRole"
-#     }]
-#   })
-# }
-
-# resource "aws_iam_role_policy_attachment" "ecr_access" {
-#   role       = aws_iam_role.ec2_ecr_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-# }
-
-# resource "aws_iam_instance_profile" "ec2_instance_profile" {
-#   name = "ec2-instance-profile"
-#   role = aws_iam_role.ec2_ecr_role.name
-# }
 
 # Amazon Linux 2023 AMI
 data "aws_ami" "amazon_linux" {
